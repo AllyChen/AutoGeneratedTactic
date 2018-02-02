ccec86e68e807b834318808d1d6868d22be27368
AllyChen <ally53628@gmail.com>
2018-02-02 18:02:27 +0800

    Write the framework of PSO.

---
+++ Assets/AutoGeneratedTactic/Scripts/PSO/ParticleSwarmOptimizationSetting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ParticleSwarmOptimizationSettingDefinition
+{
+	public class ParticleSwarmOptimizationSetting : MonoBehaviour
+	{
+
+		#region Initial
+		// Define the basic parameters.
+		private int _mapLength;
+		private int _mapWidth;
+		private int _numGenes;
+		private int _numParticles;
+		private int _numGenerations;
+
+		public void InitialPopulation(int length, int width, int numGene, int numParticle)
+		{
+
+		}
+		#endregion
+
+
+	}
+}
+
